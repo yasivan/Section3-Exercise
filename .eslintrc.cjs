0350e59e0552ed4b3013cb75b80f35a9e7fdc872
Vivek Senniappan <vivek.senniappan@cgu.com.au>
2026-06-21 20:26:06 +1000

    added lint

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    rules: {
+        'react/prop-types': 'off',
+    },
+};
